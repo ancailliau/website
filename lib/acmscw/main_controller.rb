410afda36b49bb504475e00c52741f82a4008707
BERNARD LAMBEAU <blambeau@gmail.com>
2009-11-18 17:21:17 +0100

    Removes .html files, replaced by .wtpl composed by wlang.

--- lib/acmscw/main_controller.rb
+++ lib/acmscw/main_controller.rb
@@ -1,21 +1,25 @@
 module AcmScW
   class MainController
     
-    LINK_TO_PAGES = { "/"      => 'index.html',
-                      "/latex" => 'latex.html'}
-    
-    def file_contents(file)
-      File.read(File.join(File.dirname(__FILE__), file))
-    end
-    
     def call(env)
       req = Rack::Request.new(env)
-      page = LINK_TO_PAGES[req.fullpath]
-      if page
-        [200, {'Content-Type' => 'text/html'}, [file_contents(page)]]
-      else
-        [404, {'Content-Type' => 'text/html'}, [file_contents('index.html')]]
-      end
+      
+      # find the main page to compose
+      page = req.fullpath
+      page = "/index.html" if page == '/'
+      page = $1 if page =~ /\/([a-zA-Z0-9_-]+)\.html/
+      is404 = !File.exists?(File.join(File.dirname(__FILE__), "#{page}.wtpl"))
+      page = 'index' if is404
+      
+      # compose it
+      context = {"base_href"   => AcmScW.base_href,
+                 "body_class"  => page,
+                 "pagerequest" => page}
+      layout = File.join(File.dirname(__FILE__), 'layout.wtpl')
+      composed = WLang.file_instantiate(layout, context).to_s
+      
+      # send result
+      [is404 ? 404 : 200, {'Content-Type' => 'text/html'}, [composed]]
     end
     
   end
