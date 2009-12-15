3b20a20f7c84a281e1f2497cb8c45a546cedeb2c
BERNARD LAMBEAU <blambeau@gmail.com>
2009-12-15 18:23:56 +0100

    Fixes URL bug.

--- lib/acmscw/main_controller.rb
+++ lib/acmscw/main_controller.rb
@@ -1,13 +1,25 @@
 module AcmScW
   class MainController
     
+    # The folder of this class
+    HERE = File.dirname(__FILE__)
+    
     # Finds the page file that maps a given request
     def self.find_requested_page_file(req_path)
-      req_path = "#{req_path}index" if req_path[req_path.length-1, 1] == '/'
+      # 1) Clean request
+      req_path = req_path.strip
       req_path = req_path[1..-1] if req_path[0,1]=='/'
+      req_path = req_path[0..-2] if req_path[req_path.length-1,1]=='/'
+      # 2) Main index file
+      return 'index.wtpl' if req_path.empty?
+      # 2) Check if a directory
+      if File.exists?(File.join(HERE, req_path)) and File.directory?(File.join(HERE, req_path))
+        return File.join(req_path, 'index.wtpl')
+      end
+      # 3) Looks for files now, by removing extension first
       req_path = $1 if req_path =~ /^(.*?)(\.html?)?$/
       req_path = "#{req_path}.wtpl"
-      File.exists?(File.join(File.dirname(__FILE__), req_path)) ? req_path : nil
+      File.exists?(File.join(HERE, req_path)) ? req_path : nil
     end
     
     def call(env)
