0ba5ef4f74fb2f8313373480e50545520acd4dd3
BERNARD LAMBEAU <blambeau@gmail.com>
2010-01-07 10:18:34 +0100

    Adds a basic mail server.

---
+++ lib/acmscw/tools/mail_server.rb
@@ -0,0 +1,37 @@
+require 'net/smtp'
+module AcmScW
+  module Tools
+    class MailServer
+      
+      # Sends an e-mail
+      def self.send_mail(msg, from, to)
+        if AcmScW.deploy_mode == 'production'
+          Net::SMTP.start('smtp.skynet.be', 25) do |smtp|
+            smtp.send_message msgstr, from, *to
+          end
+        else
+          to.each {|t| File.open("/tmp/#{t}.txt", 'w') {|io| io << msg}}
+        end
+      end
+      
+      # Cleans sent mail to a list of adresses.
+      # Using this method does not make sense in production mode.
+      def self.clean(*to)
+        to.each {|t| File.unlink("/tmp/#{t}.txt") if File.exists?("/tmp/#{t}.txt")}
+      end
+      
+      # Checks if a mail has been correctly sent to soe people.
+      # Using this method does not make sense in production mode.
+      def self.mail_has_been_sent?(*to)
+        to.all? {|t| File.exists?("/tmp/#{t}.txt")}
+      end
+      
+      # Returns the contents of the last mail sent to a given adress.
+      # Using this method does not make sense in production mode.
+      def self.mail_contents(to)
+        File.read("/tmp/#{to}.txt")
+      end
+      
+    end # class MailServer
+  end # module Tools
+end # module AcmScW
