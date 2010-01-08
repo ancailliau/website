352a3338280b9a95dae03f35b322247e23f7fd5c
BERNARD LAMBEAU <blambeau@gmail.com>
2010-01-08 18:42:15 +0100

    Adds a write method in Logger because of a bug using certain versions of ruby.

--- lib/waw/ext.rb
+++ lib/waw/ext.rb
@@ -1,3 +1,4 @@
+require 'logger'
 class Hash
   def keep(*keys)
     self.delete_if{|k,v| !keys.include?(k)}
@@ -7,4 +8,11 @@ class Hash
     self.each_pair {|k,v| copy[k.to_s.to_sym] = v}
     copy
   end
+end
+class Logger
+  
+  def write(*args)
+    info(*args)
+  end
+  
 end
