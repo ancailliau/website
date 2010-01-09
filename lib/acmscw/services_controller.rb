74fe7a96e810ef6cd8cc3f3308542f056dbbbc22
BERNARD LAMBEAU <blambeau@gmail.com>
2010-01-09 10:05:25 +0100

    Adds routing rules to services controller.

--- lib/acmscw/services_controller.rb
+++ lib/acmscw/services_controller.rb
@@ -26,12 +26,17 @@ module AcmScW
       validation :password, (size>=8) & (size<=15), :bad_user_or_password
       validation [:mail, :password], user_may_log, :bad_user_or_password
     }
+    routing {
+      upon 'error', 'validation_ko' do feedback end
+      upon 'success/ok'             do refresh  end
+    }
     def login(params)
       session_set(:user, params[:mail]) and :ok
     end
     
     # Lagout
     signature {}
+    routing { upon '*' do refresh end }
     def logout(params)
       session_unset(:user)
     end
@@ -40,6 +45,7 @@ module AcmScW
     
     # Subscription to the newsletter
     signature { validation :mail, mandatory & mail, :invalid_email }
+    routing { upon '*' do feedback end }
     def newsletter_subscribe(params)
       people_services.subscribe_to_newsletter(params[:mail]) and :ok
     end
@@ -57,6 +63,12 @@ module AcmScW
     signature(AccountCommonSignature) {
       validation :actkey, mandatory, :missing_activation_key
     }
+    routing {
+      upon 'error'         do feedback end
+      upon 'validation_ko' do form_validation_feedback end
+      upon 'success/ok'    do redirect(:url => '/people/my_chapter') end
+      upon 'success/activation_required' do redirect(:url => '/feedback?mkey=activation_required') end
+    }
     def activate_account(params)
       activation_key = params[:actkey]
       update_args = params.keep(:mail, :password, :newsletter, :first_name, :last_name, :occupation, :rss_feed)
@@ -70,6 +82,10 @@ module AcmScW
       validation :mail, mandatory & mail, :invalid_email 
       validation :mail, user_exists, :unknown_user
     }
+    routing {
+      upon 'error', 'validation_ko' do feedback end
+      upon 'success/ok'             do redirect(:url => '/feedback?mkey=activation_request_ok') end
+    }
     def account_activation_request(params)
       people_services.activation_request(params[:mail]) and :ok
     end
@@ -77,6 +93,12 @@ module AcmScW
     # Account creation
     signature(AccountCommonSignature) {
       validation :mail, user_not_exists, :mail_already_in_use
+    }
+    routing {
+      upon 'error'         do feedback end
+      upon 'validation_ko' do form_validation_feedback end
+      upon 'success/ok'    do redirect(:url => '/people/my_chapter') end
+      upon 'success/activation_required' do redirect(:url => '/feedback?mkey=subscribe_account_ok') end
     }
     def subscribe_account(params)
       args = params.keep(:mail, :password, :newsletter, :first_name, :last_name, :occupation, :rss_feed)
@@ -87,6 +109,12 @@ module AcmScW
     signature(AccountCommonSignature) {
       validation :mail, is_current_user | user_not_exists, :mail_already_in_use
       validation :mail, logged, :user_must_be_logged
+    }
+    routing {
+      upon 'error' do feedback end
+      upon 'validation_ko' do form_validation_feedback end
+      upon 'success/ok' do redirect(:url => '/people/my_chapter') end
+      upon 'success/activation_required' do redirect(:url => '/feedback?mkey=activation_required') end
     }
     def update_account(params)
       args = params.keep(:mail, :password, :newsletter, :first_name, :last_name, :occupation, :rss_feed)
