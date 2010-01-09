e727d9621c4959ec88b7a401f110d418e33a2539
BERNARD LAMBEAU <blambeau@gmail.com>
2010-01-10 00:03:18 +0100

    Makes wawspec tests to be executed with 'rake test' without requiring to run server manually.

--- Rakefile
+++ Rakefile
@@ -10,8 +10,13 @@ version = File.read(lib)[/^\s*VERSION\s*=\s*(['"])(\d\.\d\.\d)\1/, 2]
 task :default => [:test]
 
 desc "Lauches all tests"
-Rake::TestTask.new do |test|
+Rake::TestTask.new(:unit_test) do |test|
   test.libs       << [ "lib", "vendor/waw/lib", "test/unit"]
   test.test_files = ['test/unit/test_all.rb', 'vendor/waw/test/unit/test_all.rb']
   test.verbose    =  true
+end
+
+desc "Launches wawspec test"
+task :test => :unit_test do
+  require('test/wawspec/test_all.rb')
 end
--- test/wawspec/test_all.rb
+++ test/wawspec/test_all.rb
@@ -1,16 +1,42 @@
+# Handle load path
 here = File.dirname(__FILE__)
 top  = File.join(here, '..', '..')
 $LOAD_PATH.unshift(File.join(top, 'lib'), File.join(top, 'vendor', 'waw', 'lib'))
+
+# Makes the require
+require 'net/http'
 require 'waw'
 require 'waw/testing/wawspec'
 require 'test/unit'
 
-Waw.load_application(top)
+# Load the waw application
+app = Waw.load_application(top)
 raise "Tests cannot be run in production mode, to avoid modifying real database "\
       "or sending spam mails to real users." unless Waw.config.deploy_mode=='devel'
 
+# Load waw through rack in a different thread
+begin
+  server = Rack::Handler::Mongrel
+rescue LoadError => e
+  server = Rack::Handler::WEBrick
+end
+t = Thread.new(app) do |app|
+  options = {:Port => 9292, :Host => "0.0.0.0", :AccessLog => []}
+  server.run app, options
+end
+
+# Wait until the server is loaded
+try, ok = 0, false
+begin
+  Net::HTTP.get(URI.parse("http://localhost:9292/"))
+  ok = true
+rescue Errno::ECONNREFUSED => ex
+  sleep 1
+end until (ok or (try += 1)>3)
+
 test_files = Dir[File.join(File.dirname(__FILE__), '**/*.wawspec')]
 test_files.each { |file|
+  puts "Executing #{file}"
   load(file) 
 }
 
