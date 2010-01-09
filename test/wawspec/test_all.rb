696255f8f5e0d83d711477f0655ad30fb1b6fdb6
BERNARD LAMBEAU <blambeau@gmail.com>
2010-01-09 11:21:55 +0100

    A first integration scenario using wawspec.

--- test/wawspec/test_all.rb
+++ test/wawspec/test_all.rb
@@ -2,14 +2,14 @@ here = File.dirname(__FILE__)
 top  = File.join(here, '..', '..')
 $LOAD_PATH.unshift(File.join(top, 'lib'), File.join(top, 'vendor', 'waw', 'lib'))
 require 'acmscw'
-require 'test/unit'
+require 'waw/testing/wawspec'
 
 AcmScW.load_configuration_file
 raise "Tests cannot be run in production mode, to avoid modifying real database "\
       "or sending spam mails to real users." unless AcmScW.deploy_mode=='devel'
 
-test_files = Dir[File.join(File.dirname(__FILE__), '**/*_test.rb')]
+test_files = Dir[File.join(File.dirname(__FILE__), '**/*.wawspec')]
 test_files.each { |file|
-  require(file) 
+  load(file) 
 }
 
