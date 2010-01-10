4284cfaa1d4498a36f31132fffad13b54a6941d0
BERNARD LAMBEAU <blambeau@gmail.com>
2010-01-10 21:03:32 +0100

    Moves from wlang 0.8.1 to 0.8.2

--- resources/services.rs
+++ resources/services.rs
@@ -1,3 +1,3 @@
 # About waw services
-public        Waw::Services::PublicPages.new
-people        Waw::Services::JSONServices.new('/services' => AcmScW::ServicesController.new)
+public_pages Waw::Services::PublicPages.new
+people       Waw::Services::JSONServices.new('/services' => AcmScW::ServicesController.new)
