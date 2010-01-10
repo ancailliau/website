f92da8d6525ce9981c4f8f7050cc011f1996282e
BERNARD LAMBEAU <blambeau@gmail.com>
2010-01-10 16:55:43 +0100

    Adds a simple security test.

--- test/wawspec/test_all.rb
+++ test/wawspec/test_all.rb
@@ -8,6 +8,9 @@ require 'net/http'
 require 'waw'
 require 'waw/testing/wawspec'
 
+puts "Running wawspec suite (#{File.expand_path(here)})"
+puts "Loading waw application and web server"
+
 # Load the waw application
 app = Waw.load_application(top)
 raise "Tests cannot be run in production mode, to avoid modifying real database "\
@@ -30,15 +33,16 @@ begin
   Net::HTTP.get(URI.parse(Waw.config.web_base))
   ok = true
 rescue Errno::ECONNREFUSED => ex
-  sleep 1
+  sleep 0.1
 end until (ok or (try += 1)>3)
 
+puts "Running wawspec tests now"
 t1 = Time.now
-puts "Running wawspec suite (#{File.expand_path(here)})"
 Dir[File.join(here, '**/*.wawspec')].each {|f| load(f)}
 tt, ta, tf, te = 0, 0, 0, 0
 Waw::WawSpec::SCENARIOS.each { |name, scenario|
   STDOUT.write "."
+  STDOUT.flush
   begin
     scenario.run
     tt += 1
