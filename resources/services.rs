db705b877f58c8ddc54f14d955bda66d731c3c40
BERNARD LAMBEAU <blambeau@gmail.com>
2010-01-12 18:05:17 +0100

    Adds a FileValidator. Moves templates elsewhere to prepare wawaccess being ready.

--- resources/services.rs
+++ resources/services.rs
@@ -1,4 +1,5 @@
 # About waw services
+#public_pages Waw::Services::PublicPages::WawAccess.load_hierarchy('public')
 public_pages Waw::Services::PublicPages::Controller.new
 webserv      Waw::Services::JSONServices.new('/webserv/people' => AcmScW::Controllers::PeopleController.new,
                                              '/webserv/event' => AcmScW::Controllers::EventController.new)
