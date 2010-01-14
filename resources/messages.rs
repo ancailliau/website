9ba735381bbdc7c4083325677956ee54e417bf42
Bernard Lambeau <blambeau@gmail.com>
2010-01-14 18:34:53 +0100

    Modifies different formularies and pages, during meeting with CA.

--- resources/messages.rs
+++ resources/messages.rs
@@ -2,16 +2,16 @@
 server_error                "Une erreur est survenue. Veuillez réessayer plus tard"
 
 # Validation messages
-invalid_email               "Adresse email invalide"
+invalid_email               "Adresse e-mail invalide"
 missing_activation_key      "Clé d'activation manquante"
 passwords_dont_match        "Les mots de passe ne correspondent pas"
 bad_newsletter              "L'inscription a la newsletter est invalide"
 bad_password                "Votre mot de passe doit comporter entre 8 et 15 caractères"
 bad_rss_feed                "Votre flux RSS ne semble pas être une adresse web valide"
 bad_user_or_password        "Utilisateur ou mot de passe invalide (<a href='/people/lost_password'>perdu?</a>)"
-mail_already_in_use         "Cette adresse email est déjà utilisée"
+mail_already_in_use         "Cette adresse e-mail est déjà utilisée"
 user_must_be_logged         "Vous devez être connecté pour accéder/modifier ces informations"
-unknown_user                "Cette adresse email est inconnue dans notre base de données"
+unknown_user                "Cette adresse e-mail est inconnue dans notre base de données"
 unknown_event               "Cet événement n'est pas connu"
 
 # Ok feedback messages
@@ -24,10 +24,10 @@ click_here_to_unregister          "Cliquez-ici pour vous désinscrire"
 
 # Redirect feedback messages
 update_account_ok           "<p>Les informations vous concernant ont été mises à jour avec succès!</p>"
-activation_request_ok       "<p>Un mail vous permettant de réactiver votre compte vous a été envoyé. Veuillez suivre les instructions s'y trouvant.</p>"
-subscribe_account_ok        "<p>Un mail vous permettant d'activer votre compte vous a été envoyé. Veuillez suivre les instructions s'y trouvant.</p>"
+activation_request_ok       "<p>Un e-mail vous permettant de réactiver votre compte vous a été envoyé. Veuillez suivre les instructions s'y trouvant.</p>"
+subscribe_account_ok        "<p>Un e-mail vous permettant d'activer votre compte vous a été envoyé. Veuillez suivre les instructions s'y trouvant.</p>"
 activation_required         "<p>Vous avez modifié votre adresse e-mail. Vous devez maintenant réactiver votre compte."\
-                            "<p>Un mail vous permettant d'activer votre compte vous a été envoyé. Veuillez suivre les instructions s'y trouvant.</p>"
+                            "<p>Un e-mail vous permettant d'activer votre compte vous a été envoyé. Veuillez suivre les instructions s'y trouvant.</p>"
 
 
 # About olympiades
