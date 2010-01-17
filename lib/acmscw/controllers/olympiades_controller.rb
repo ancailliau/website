20badd0f9a237ca7281f743b0046300260251e4c
Bernard Lambeau <blambeau@gmail.com>
2010-01-17 12:06:51 +0100

    Lightweight changes for the validation branch of waw.

--- lib/acmscw/controllers/olympiades_controller.rb
+++ lib/acmscw/controllers/olympiades_controller.rb
@@ -14,7 +14,7 @@ module AcmScW
         validation [:gender, :first_name, :last_name, :mail, :birthdate, :street, :postal_code,
                     :city, :school_name, :school_city, :category, :center], mandatory, :olympiades_mandatory_fields
         validation :mail, mail, :invalid_email
-        validation :birthdate, /\d\d\/\d\d\/\d\d\d\d/.to_validator, :invalid_birthdate
+        validation :birthdate, /\d\d\/\d\d\/\d\d\d\d/, :invalid_birthdate
       }
       routing {
         upon 'validation-ko' do form_validation_feedback end
--- lib/acmscw/waw_ext/validations.rb
+++ lib/acmscw/waw_ext/validations.rb
@@ -1,30 +1,16 @@
 module Waw
   module Validation
 
-    # Validator for user existence
-    UserExists = Waw::Validation.validator {|mail| Waw.resources.business.people.people_exists?(mail)}
-    def self.user_exists() UserExists end
-    def self.user_not_exists() UserExists.not end
+   validator :user_exists,      validator {|mail| Waw.resources.business.people.people_exists?(mail)}
+   validator :user_not_exists,  user_exists.not
+   validator :user_may_log,     validator {|mail, pass| Waw.resources.business.people.people_may_log?(mail, pass)}
 
-    # Validator for user login
-    UserMayLog = Waw::Validation.validator do |mail, pass|
-      Waw.resources.business.people.people_may_log?(mail, pass)
-    end
-    def self.user_may_log() UserMayLog end
-      
-    # Validation that a parameter matches the current user
-    IsCurrentUser = Waw::Validation.validator{|mail| mail==Waw.session_has_key?(:user)}
-    def self.is_current_user() IsCurrentUser end
+   validator :is_current_user,  validator{|mail| mail==Waw.session_has_key?(:user)}
+   validator :logged,           validator{|*args| Waw.session_has_key?(:user)}
+   validator :not_logged,       logged.not
+   
+   validator :event_exists,     validator {|event| Waw.resources.business.event.event_exists?(event)}  
+   validator :event_not_exists, event_exists.not
   
-    # Checks that user is (not) logged
-    UserIsLogged = Waw::Validation.validator{|*args| Waw.session_has_key?(:user)}
-    def self.logged() UserIsLogged end
-    def self.not_logged() UserIsLogged.not end
-    
-    # Validator for event existence
-    EventExists = Waw::Validation.validator {|event| Waw.resources.business.event.event_exists?(event)}
-    def self.event_exists() EventExists end
-    def self.event_not_exists() EventExists.not end
-      
   end
 end
