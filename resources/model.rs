c62f315c4ab13a490a34f677e3c57b5d1ecfeeeb
Bernard Lambeau <blambeau@gmail.com>
2010-02-01 11:16:08 +0100

    Creates a basic page allowing to see olympiades registrations.

--- resources/model.rs
+++ resources/model.rs
@@ -1,3 +1,4 @@
 people          AcmScW.database[:people]
 activities      AcmScW.database[:activities]
 planned_events  AcmScW.database[:events].filter(:status => 'planned')
+olympiades_registrations AcmScW.database[:olympiades_registrations]
