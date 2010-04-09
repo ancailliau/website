6832bb351849efe6598fc38f7e56a2fcfb64f5f9
Damien Leroy <damien.leroy@uclouvain.be>
2010-04-09 12:56:38 +0200

    Refs#449

--- lib/acmscw/business/olympiades_services.rb
+++ lib/acmscw/business/olympiades_services.rb
@@ -18,8 +18,8 @@ module AcmScW
       
       # Sends an email to participant with URL to their points
       def send_results_announce_mail
-        Waw.resources.model.olympiades_results.select{|p| p[:mail] != "" }.each do |people|
-          mail, sid = people[:mail], people[:sid]
+        Waw.resources.model.olympiades_results.select{|p| people[:email] != "" }.each do |people|
+          mail, sid = people[:email], people[:sid]
           context = {:url => Waw.config.web_base + "olympiades/resultats-demi-finales/show/#{sid}"}
           mail_agent.send_mail(:olympiades_results_announce, context, mail)
         end
