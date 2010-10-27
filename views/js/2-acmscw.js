f761f2b7204c851bcf9b9026765e17ea095f4368
Bernard Lambeau <blambeau@gmail.com>
2010-10-27 15:32:40 +0200

    Starts refactoring of forms.

---
+++ views/js/2-acmscw.js
@@ -0,0 +1,20 @@
+function show_popup(url, width) {
+	$.get(url, function(data) { 
+		$('#popup').html(data); 
+	  $('#popup').width(width);
+		$('#popup').css("margin-left", "-" + (width/2) + "px");
+		$('#popup').css("top", (50+$(window).scrollTop()) + "px");
+		inside_height = $('#inside').height()+100;
+		window_height = $(window).height();
+		$('#hide').height(inside_height > window_height ? inside_height : window_height);
+		$('#hide').width($(window).width());
+	  $('#hide').show();
+		$('#popup').show();
+    $('html, body').animate( { scrollTop: 0 }, 'slow' );
+	});
+}
+function hide_popup(refresh) {
+	$('#popup').hide();
+  $('#hide').hide();
+  if (refresh) { location.reload(true); }
+}  
