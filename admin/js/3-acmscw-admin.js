12846cb286c17c8dc4434ebbec59ccb84842f796
Bernard Lambeau <blambeau@gmail.com>
2010-10-29 18:15:00 +0200

    Administration continued.

--- admin/js/3-acmscw-admin.js
+++ admin/js/3-acmscw-admin.js
@@ -1,6 +1,6 @@
-function open_dataset(url, label) {
+function open_tab(url, label) {
 	tabs = $("#admin-tabs");
-	tabs.tabs('add', '/dba/' + url, label);
+	tabs.tabs('add', url + "?jsrequest=true", label);
 	tabs.tabs('select', tabs.tabs('length')-1);
 	$("#admin-tabs table").fixedHeaderTable();
 }
--- resources/model.rs
+++ resources/model.rs
@@ -1,7 +1,10 @@
 people                   AcmScW.database[:people]
 activities               AcmScW.database[:activities]
+events                   AcmScW.database[:events]
 planned_events           AcmScW.database[:events].filter(:status => 'planned')
 olympiades_registrations AcmScW.database[:olympiades_registrations]
 olympiades_results       AcmScW.database[:olympiades_results]
 olympiades_results_sec   AcmScW.database[:olympiades_results].filter(:cat => '1').filter(:selectionne => 'true').order(:nom)
 olympiades_results_sup   AcmScW.database[:olympiades_results].filter(:cat => '2').filter(:selectionne => 'true').order(:nom)
+webr_past_events         AcmScW.database[:webr_past_events]
+webr_planned_events      AcmScW.database[:webr_planned_events]
