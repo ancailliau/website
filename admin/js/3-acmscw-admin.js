5b9d3f1ca758af671b30da2c3388f295f0590389
Bernard Lambeau <blambeau@gmail.com>
2010-10-29 18:32:50 +0200

    Avoid re-opening the same tab twice.

--- admin/js/3-acmscw-admin.js
+++ admin/js/3-acmscw-admin.js
@@ -1,6 +1,15 @@
 function open_tab(url, label) {
 	tabs = $("#admin-tabs");
-	tabs.tabs('add', url + "?jsrequest=true", label);
-	tabs.tabs('select', tabs.tabs('length')-1);
-	$("#admin-tabs table").fixedHeaderTable();
+	found = false;
+	for (i=0; i<tabs.tabs("length"); i++){
+		if ($("#admin-tabs li:eq(" + i + ") a").html() == label) {
+    	tabs.tabs('select', i);
+      found = true;
+			break;
+		}
+	}
+	if (!found) {
+		tabs.tabs('add', url + "?jsrequest=true", label);
+		tabs.tabs('select', tabs.tabs('length')-1);
+	}
 }
